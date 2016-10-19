904f91e77c070472ad35499c44c1897d397cc73b
maditnerd <remi@madnerd.org>
2016-10-19 15:06:22 +0200

    First autocommit by pigetBot

---
+++ wavPlayer/wavPlayer.ino
@@ -0,0 +1,141 @@
+/*
+wavPlayer
+Madnerd.org
+
+Primitive wav file player
+Only read : Wav Unsigned 8-bit PCM file
+Track will stopped if saturation
+
+If you want to make a real mp3 player , use a DFPlayer Mini instead
+This is just a proof of concept.
+
+Components
+- Micro SD card adapter
+- Headphone input
+- 2 buttons
+- Arduino mini pro 3v
+
+* Open MP3 with Audacity
+* File/Export Audio
+* Save as type: Other uncompressed files
+* Header: Wav (Microsoft)
+* Encoding : Unsigned 8_bit PCM
+* Save to 0.wav
+
+Wiring:
+SD card 
+1 --> X
+2 --> 12
+3 --> GND
+4 --> 13
+5 --> VCC
+6 --> GND
+7 --> 11
+8 --> 10 (Chip select)
+
+Audio: 6
+Button Previous track: 7
+Button Next track:8
+
+*/
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+const int b_prev = 8;
+const int b_next = 7;
+int file = 0;
+bool playing = true;
+String string_filename;
+char* filename;
+int check_buttons = 0;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  pinMode(6, OUTPUT);
+  TCCR0B = 0x01; // Sets the PWM frequency to 64KHz in pins 5 and 6.
+  //while (!Serial) {
+  //  ; // wait for serial port to connect. Needed for native USB port only
+  //}
+
+
+  Serial.print("Initializing SD card...");
+  while (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+  }
+
+  Serial.println("initialization done.");
+
+  //Enable buttons
+  pinMode(b_prev, INPUT_PULLUP);
+  pinMode(b_next, INPUT_PULLUP);
+
+}
+
+void loop() {
+  //Convert string to char
+  string_filename = String(file) + ".wav";
+  unsigned int bufSize = string_filename.length() + 1; //String length + null terminator
+  filename = new char[bufSize];
+  string_filename.toCharArray(filename, bufSize);
+
+  if (SD.exists(filename)) {
+    Serial.println(string_filename);
+    myFile = SD.open(filename, FILE_READ);
+  }
+  else {
+    Serial.println(string_filename + "doesn't exists");
+    file = 0;
+  }
+
+
+  unsigned char data;
+  for (int count = 0; count < 128; count++) {
+    data = myFile.read();
+  }
+  Serial.println("Start");
+  playing = true;
+  check_buttons = 0;
+  while (data != 255) {
+    data = myFile.read();
+    analogWrite(6, data);
+   // delayMicroseconds(10);
+    checkButtons();
+
+    if (!playing) {
+      break;
+    }
+  }
+
+
+  //Serial.println("Stop");
+  myFile.close();
+  Serial.println("Replay");
+  // nothing happens after setup finishes.
+  delay(1000);
+
+}
+
+void checkButtons() {
+  check_buttons++;
+  //We reduce check because our arduino is really time sensitive when reading wav files.
+  if (check_buttons == 1000) {
+    check_buttons = 0;
+    if (!digitalRead(b_prev)) {
+      Serial.println("Button previous");
+      file--;
+      playing = false;
+      delay(1000);
+    }
+
+    if (!digitalRead(b_next)) {
+      Serial.println("Button next");
+      file++;
+      playing = false;
+      delay(1000);
+    }
+  }
+}
+
+
